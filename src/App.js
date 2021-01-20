a8b9b35a89e27c71dcad2f2663cd0bddbac143de
Kevin Ziechmann <kziechmann@gmail.com>
2021-01-19 21:25:24 -0800

    Initial setup with truffle.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Web3 from 'web3';
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
 
 function App() {
+  
+
+  const [account, setAccount ] = React.useState('')
+  const [todoList, setToDoList ] = React.useState([])
+
+  React.useEffect(() =>{
+    const loadBlockChainData = async () => {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+      const network = await web3.eth.net.getNetworkType()
+      const accounts = await web3.eth.getAccounts()
+      const toDoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+      return { accounts, toDoList }
+    }
+    const { accounts = [], toDoList } = loadBlockChainData()
+    setAccount(accounts[0] || 'fakeAddress')
+    setToDoList(toDoList)
+  }, [])
+
+  console.log(account, todoList)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Hello world</h1>
+      <h2> your account: {account} </h2>
+        <ul>
+          {todoList && todoList.map(todo => <li>{todo}</li>)}
+        </ul>
     </div>
   );
 }
